093ad61a3c4fd720fd200e26b819caac444e6781
Amir Amin <amin.amir@gmail.com>
2018-03-15 06:05:40 -0400

    intermediate commit

--- examples/hellotinyb.cpp
+++ examples/hellotinyb.cpp
@@ -71,125 +71,118 @@ int main(int argc, char **argv)
     /* Start the discovery of devices */
     bool ret = manager->start_discovery();
     std::cout << "Started = " << (ret ? "true" : "false") << std::endl;
-
-    BluetoothDevice *sensor_tag = NULL;
-    BluetoothGattService *temperature_service = NULL;
-
-    for (int i = 0; i < 15; ++i) {
-        std::cout << "Discovered devices: " << std::endl;
-        /* Get the list of devices */
-        auto list = manager->get_devices();
-
-        for (auto it = list.begin(); it != list.end(); ++it) {
-
-            std::cout << "Class = " << (*it)->get_class_name() << " ";
-            std::cout << "Path = " << (*it)->get_object_path() << " ";
-            std::cout << "Name = " << (*it)->get_name() << " ";
-            std::cout << "Connected = " << (*it)->get_connected() << " ";
-            std::cout << "RSSI = " << (*it)->get_rssi() << " ";
-            std::cout << std::endl;
-            
-            if(argc >= 2)
-            {
-				if ((*it)->get_address() == argv[1])
-					sensor_tag = (*it).release();
-			}
-            /* Search for the device with the address given as a parameter to the program */
-            else if ((*it)->get_name() == "Intech_BLE")
-            {
-                sensor_tag = (*it).release();
-			}
-        }
-
-        /* Free the list of devices and stop if the device was found */
-        if (sensor_tag != nullptr)
-            break;
-        /* If not, wait and try again */
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-        std::cout << std::endl;
-    }
-
-    /* Stop the discovery (the device was found or number of tries ran out */
-    ret = manager->stop_discovery();
-    std::cout << "Stopped = " << (ret ? "true" : "false") << std::endl;
-
-    if (sensor_tag == nullptr) {
-        std::cout << "Could not find device " << argv[1] << std::endl;
-        return 1;
-    }
-    
-    int discovered = 0;
-    BluetoothGattCharacteristic *temp_value = nullptr;
-    BluetoothGattCharacteristic *temp_config = nullptr;
-    //BluetoothGattCharacteristic *temp_period = nullptr;
     
     while (running)
-    {
-		
-		std::cout << "RSSI = " << sensor_tag->get_rssi() << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+    {    
 
-		/* Connect to the device and get the list of services exposed by it */
-		sensor_tag->connect();
-		
-		if (discovered == 0)
-		{
-			std::cout << "Discovered services: " << std::endl;
-			while (true) {
-				/* Wait for the device to come online */
-				std::this_thread::sleep_for(std::chrono::seconds(4));
+		BluetoothDevice *sensor_tag = NULL;
+		BluetoothGattService *temperature_service = NULL;
 
-				auto list = sensor_tag->get_services();
-				if (list.empty())
-					continue;
+		//wait indefinitely for the device
+		while (true) { 
+			std::cout << "Discovered devices: " << std::endl;
+			/* Get the list of devices */
+			auto list = manager->get_devices();
 
-				for (auto it = list.begin(); it != list.end(); ++it) {
-					std::cout << "Class = " << (*it)->get_class_name() << " ";
-					std::cout << "Path = " << (*it)->get_object_path() << " ";
-					std::cout << "UUID = " << (*it)->get_uuid() << " ";
-					std::cout << "Device = " << (*it)->get_device().get_object_path() << " ";
-					std::cout << std::endl;
-
-					/* Search for the temperature service, by UUID */
-					if ((*it)->get_uuid() == "77880001-b5a3-f393-e0a9-150e24fcca8e")
-						temperature_service = (*it).release();
-				}
-				break;
-			}
-
-			if (temperature_service == nullptr) {
-				std::cout << "Could not find service 77880001-b5a3-f393-e0a9-150e24fcca8e" << std::endl;
-				return 1;
-			}
-
-
-			/* If there is a temperature service on the device with the given UUID,
-			 * get it's characteristics, by UUID again */
-			auto list = temperature_service->get_characteristics();
-			std::cout << "Discovered characteristics: " << std::endl;
 			for (auto it = list.begin(); it != list.end(); ++it) {
 
 				std::cout << "Class = " << (*it)->get_class_name() << " ";
 				std::cout << "Path = " << (*it)->get_object_path() << " ";
+				std::cout << "Name = " << (*it)->get_name() << " ";
+				std::cout << "Connected = " << (*it)->get_connected() << " ";
+				std::cout << "RSSI = " << (*it)->get_rssi() << " ";
+				std::cout << std::endl;
+				
+				if(argc >= 2)
+				{
+					if ((*it)->get_address() == argv[1])
+						sensor_tag = (*it).release();
+				}
+				/* Search for the device with the address given as a parameter to the program */
+				else if ((*it)->get_name() == "Intech_BLE")
+				{
+					sensor_tag = (*it).release();
+				}
+			}
+
+			/* Free the list of devices and stop if the device was found */
+			if (sensor_tag != nullptr)
+				break;
+			/* If not, wait and try again */
+			std::this_thread::sleep_for(std::chrono::seconds(4));
+			std::cout << std::endl;
+		}
+		
+		std::cout << "RSSI = " << sensor_tag->get_rssi() << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		
+
+		/* Stop the discovery (the device was found or number of tries ran out */
+		/*ret = manager->stop_discovery();
+		std::cout << "Stopped = " << (ret ? "true" : "false") << std::endl;*/
+
+		int discovered = 0;
+		BluetoothGattCharacteristic *temp_value = nullptr;
+		BluetoothGattCharacteristic *temp_config = nullptr;
+		//BluetoothGattCharacteristic *temp_period = nullptr;
+    
+		/* Connect to the device and get the list of services exposed by it */
+		sensor_tag->connect();		
+
+		std::cout << "Discovered services: " << std::endl;
+		while (true) {
+			/* Wait for the device to come online */
+			std::this_thread::sleep_for(std::chrono::seconds(4));
+
+			auto list = sensor_tag->get_services();
+			if (list.empty())
+				continue;
+
+			for (auto it = list.begin(); it != list.end(); ++it) {
+				std::cout << "Class = " << (*it)->get_class_name() << " ";
+				std::cout << "Path = " << (*it)->get_object_path() << " ";
 				std::cout << "UUID = " << (*it)->get_uuid() << " ";
-				std::cout << "Service = " << (*it)->get_service().get_object_path() << " ";
+				std::cout << "Device = " << (*it)->get_device().get_object_path() << " ";
 				std::cout << std::endl;
 
-				if ((*it)->get_uuid() == "77880003-b5a3-f393-e0a9-150e24fcca8e")
-					temp_value = (*it).release();
-				else if ((*it)->get_uuid() =="77880002-b5a3-f393-e0a9-150e24fcca8e")
-					temp_config = (*it).release();
-				/*else if ((*it)->get_uuid() == "f000aa03-0451-4000-b000-000000000000")
-					temp_period = (*it).release();*/
+				/* Search for the temperature service, by UUID */
+				if ((*it)->get_uuid() == "77880001-b5a3-f393-e0a9-150e24fcca8e")
+					temperature_service = (*it).release();
 			}
-
-			if (temp_config == nullptr || temp_value == nullptr /*|| temp_period == nullptr */) {
-				std::cout << "Could not find characteristics." << std::endl;
-				return 1;
-			}
-			
-			discovered = 1;
+			break;
 		}
+
+		if (temperature_service == nullptr) {
+			std::cout << "Could not find service 77880001-b5a3-f393-e0a9-150e24fcca8e" << std::endl;
+			return 1;
+		}
+
+
+		 /* get it's characteristics, by UUID again */
+		auto list = temperature_service->get_characteristics();
+		std::cout << "Discovered characteristics: " << std::endl;
+		for (auto it = list.begin(); it != list.end(); ++it) {
+
+			std::cout << "Class = " << (*it)->get_class_name() << " ";
+			std::cout << "Path = " << (*it)->get_object_path() << " ";
+			std::cout << "UUID = " << (*it)->get_uuid() << " ";
+			std::cout << "Service = " << (*it)->get_service().get_object_path() << " ";
+			std::cout << std::endl;
+
+			if ((*it)->get_uuid() == "77880003-b5a3-f393-e0a9-150e24fcca8e")
+				temp_value = (*it).release();
+			else if ((*it)->get_uuid() =="77880002-b5a3-f393-e0a9-150e24fcca8e")
+				temp_config = (*it).release();
+			/*else if ((*it)->get_uuid() == "f000aa03-0451-4000-b000-000000000000")
+				temp_period = (*it).release();*/
+		}
+
+		if (temp_config == nullptr || temp_value == nullptr /*|| temp_period == nullptr */) {
+			std::cout << "Could not find characteristics." << std::endl;
+			return 1;
+		}
+			
+
 
 		try {
             /* Read temperature data and display it */
@@ -199,18 +192,6 @@ int main(int argc, char **argv)
             if (size > 0) {
                 data = response.data();
 
-                /*std::cout << "Raw data=[";
-                for (unsigned i = 0; i < response.size(); i++)
-                    std::cout << std::hex << static_cast<int>(data[i]) << ", ";
-                std::cout << "] " << std:: dec;*/
-
-
-                /*uint16_t ambient_temp, object_temp;
-                object_temp = data[0] | (data[1] << 8);
-                ambient_temp = data[2] | (data[3] << 8);
-
-                std::cout << "Ambient temp: " << celsius_temp(ambient_temp) << "C ";
-                std::cout << "Object temp: " << celsius_temp(object_temp) << "C "; */
                 std::cout << std::endl;
                 int b0 = static_cast<int>(data[0]); 
                 int b1 = static_cast<int>(data[1]);
